dca1eb979c0992e92af9b80a821937c159f59b05
dsxkline <934476300@qq.com>
2023-02-14 21:22:24 +0800

    Update main.py

--- 1、K线周期转换算法/main.py
+++ 1、K线周期转换算法/main.py
@@ -207,7 +207,6 @@ class DsxBaseConverKlines:
         """
         return self.converkline_to(cycle)
 
-
 if __name__=="__main__":
     
     with open(os.path.dirname(os.path.abspath(__file__))+"/day.txt") as f:
